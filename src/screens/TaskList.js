32646599ee302e2615349dfeaa9155d06fc1a71e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-14 16:24:41 -0300

    72. Componente Lista de Tarefas #04

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -5,6 +5,7 @@ import todayImage from '../../assets/imgs/today.jpg'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
+import commonStyles from '../commonStyles'
 
 export default class TaskList extends Component {
   render() {
@@ -15,12 +16,14 @@ export default class TaskList extends Component {
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.titleBar}>
-            <Text>Hoje</Text>
-            <Text>{today}</Text>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Text>TaskList</Text>
+          <Text>Tarefa #01</Text>
+          <Text>Tarefa #02</Text>
+          <Text>Tarefa #03</Text>
         </View>
       </View>
     )
@@ -40,5 +43,19 @@ const styles = StyleSheet.create({
   titleBar: {
     flex: 1,
     justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
   },
 })
