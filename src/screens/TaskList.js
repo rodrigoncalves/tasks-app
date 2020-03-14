ffeaaee834f498b92aeb8bed3528239514cd4939
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-14 16:36:14 -0300

    73. Componente Tarefa #01

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -6,6 +6,7 @@ import todayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles'
+import Task from '../components/Task'
 
 export default class TaskList extends Component {
   render() {
@@ -21,9 +22,12 @@ export default class TaskList extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Text>Tarefa #01</Text>
-          <Text>Tarefa #02</Text>
-          <Text>Tarefa #03</Text>
+          <Task
+            desc="Comprar Livro"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Ler livro Livro" estimateAt={new Date()} />
         </View>
       </View>
     )
