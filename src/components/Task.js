c7d42a3e8bad1a8506a7ac2ad7da59907fa2d086
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-14 23:43:18 -0300

    75. Componente Tarefa #03

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props => {
   return (
     <View style={styles.container}>
-      <Text>{props.desc}</Text>
-      <Text>{props.estimateAt + ''}</Text>
-      <Text>{props.doneAt + ''}</Text>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text>{props.desc}</Text>
+        <Text>{props.estimateAt + ''}</Text>
+      </View>
     </View>
   )
+}
+
+function getCheckView(doneAt) {
+  if (doneAt) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={20} color="#fff" />
+      </View>
+    )
+  }
+
+  return <View style={styles.pending} />
 }
 
 const styles = StyleSheet.create({
@@ -18,5 +33,25 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10,
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4d7031',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
