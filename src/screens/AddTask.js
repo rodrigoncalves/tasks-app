99d420c4f0749fed95685b3ba8fb0aef23364467
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-15 01:29:03 -0300

    83. Modal Adicionar Tarefa #02

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react'
-import {Modal, View, StyleSheet, TouchableWithoutFeedback} from 'react-native'
+import {
+  Modal,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native'
+import commonStyles from '../commonStyles'
 
 export default class AddTask extends Component {
   render() {
@@ -10,7 +17,13 @@ export default class AddTask extends Component {
         onRequestClose={this.props.onCancel}
         animationType="slide">
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
-          <View style={styles.background}></View>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <Text style={styles.header}>Nova Tarefa</Text>
+        </View>
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.background} />
         </TouchableWithoutFeedback>
       </Modal>
     )
@@ -18,5 +31,17 @@ export default class AddTask extends Component {
 }
 
 const styles = StyleSheet.create({
-  background: {flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.7)'},
+  background: {flex: 2, backgroundColor: 'rgba(0, 0, 0, 0.7)'},
+  container: {
+    flex: 3,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today,
+    color: commonStyles.colors.secondary,
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 15,
+  },
 })
