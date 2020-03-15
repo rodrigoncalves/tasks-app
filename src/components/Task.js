387cc8643bb60c537c2d2c3160072253746352a7
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-14 23:57:59 -0300

    77. Componente Tarefa #05

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,16 +3,24 @@ import {View, Text, StyleSheet} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import commonStyles from '../commonStyles'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 export default props => {
   const doneOrNotStyle = props.doneAt
     ? {textDecorationLine: 'line-through'}
     : {}
+
+  const date = props.doneAt ? props.doneAt : props.estimateAt
+  const formattedDate = moment(date)
+    .locale('pt-br')
+    .format('ddd, D [de] MMMM')
   return (
     <View style={styles.container}>
       <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
       <View>
         <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-        <Text>{props.estimateAt + ''}</Text>
+        <Text style={styles.date}>{formattedDate}</Text>
       </View>
     </View>
   )
@@ -62,5 +70,10 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.mainText,
     fontSize: 15,
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 12,
   },
 })
