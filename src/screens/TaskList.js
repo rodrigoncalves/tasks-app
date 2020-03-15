374bf9c334e82cb57500d40aa22914fc38cf3dba
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-15 00:29:01 -0300

    80. Botão Filtrar Tarefas

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react'
-import {View, Text, ImageBackground, StyleSheet, FlatList} from 'react-native'
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import todayImage from '../../assets/imgs/today.jpg'
+import commonStyles from '../commonStyles'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
-import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 
 export default class TaskList extends Component {
   state = {
+    showDoneTasks: true,
     tasks: [
       {
         id: Math.random(),
@@ -24,6 +35,10 @@ export default class TaskList extends Component {
         doneAt: null,
       },
     ],
+  }
+
+  toggleFilter = () => {
+    this.setState({showDoneTasks: !this.state.showDoneTasks})
   }
 
   toggleTask = taskId => {
@@ -44,6 +59,15 @@ export default class TaskList extends Component {
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.iconBar}>
+            <TouchableOpacity onPress={this.toggleFilter}>
+              <Icon
+                name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                size={20}
+                color={commonStyles.colors.secondary}
+              />
+            </TouchableOpacity>
+          </View>
           <View style={styles.titleBar}>
             <Text style={styles.title}>Hoje</Text>
             <Text style={styles.subtitle}>{today}</Text>
@@ -90,5 +114,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 20,
     marginBottom: 30,
+  },
+  iconBar: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    justifyContent: 'flex-end',
+    marginTop: Platform.OS === 'ios' ? 40 : 10,
   },
 })
