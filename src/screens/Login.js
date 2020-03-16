1182073c9a397ae322e1570f6df7a2a159288d6c
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-16 01:56:17 -0300

    113. Tela de Autenticação #02

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react'
-import {ImageBackground, Text, StyleSheet} from 'react-native'
+import {ImageBackground, Text, StyleSheet, View, Platform} from 'react-native'
 
 import bgImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler'
 
 export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
   render() {
     return (
       <ImageBackground source={bgImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="E-mail"
+            value={this.state.email}
+            style={styles.input}
+            onChangeText={email => this.setState({email})}
+          />
+          <TextInput
+            placeholder="Senha"
+            value={this.state.password}
+            style={styles.input}
+            onChangeText={password => this.setState({password})}
+          />
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     )
   }
@@ -26,5 +51,26 @@ const styles = StyleSheet.create({
     color: commonStyles.colors.secondary,
     fontSize: 70,
     marginBottom: 10,
+  },
+  form: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  input: {
+    marginTop: 10,
+    backgroundColor: 'white',
+    padding: Platform.OS === 'ios' ? 15 : 10,
+  },
+  button: {
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontFamily: commonStyles.fontFamily,
+    color: 'white',
+    fontSize: 20,
   },
 })
