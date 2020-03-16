6c48f9091d7af3df593a0745ddfa0b6534eb77d3
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-16 01:45:55 -0300

    112. Tela de Autenticação #01

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native'
-import TaskList from './src/screens/TaskList'
+import Login from './src/screens/Login'
 import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => TaskList)
+AppRegistry.registerComponent(appName, () => Login)
