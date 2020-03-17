ea9a3e1155b4e6e7a40374fc9f95b356acba0233
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-16 22:01:09 -0300

    128. Drawer Navigator #03

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -18,6 +18,9 @@ import 'moment/locale/pt-br'
 
 import Task from '../components/Task'
 import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 import commonStyles from '../commonStyles'
 import AddTask from './AddTask'
 import {server, showError} from '../common'
@@ -123,6 +126,32 @@ export default class TaskList extends Component {
     }
   }
 
+  getImage = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return todayImage
+      case 1:
+        return tomorrowImage
+      case 7:
+        return weekImage
+      default:
+        return monthImage
+    }
+  }
+
+  getColor = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return commonStyles.colors.today
+      case 1:
+        return commonStyles.colors.tomorrow
+      case 7:
+        return commonStyles.colors.week
+      default:
+        return commonStyles.colors.month
+    }
+  }
+
   render() {
     const today = moment()
       .locale('pt-br')
@@ -134,7 +163,7 @@ export default class TaskList extends Component {
           onCancel={() => this.setState({showAddTask: false})}
           onSave={this.addTask}
         />
-        <ImageBackground source={todayImage} style={styles.background}>
+        <ImageBackground source={this.getImage()} style={styles.background}>
           <View style={styles.iconBar}>
             <TouchableOpacity
               onPress={() => this.props.navigation.openDrawer()}>
@@ -171,7 +200,7 @@ export default class TaskList extends Component {
           />
         </View>
         <TouchableOpacity
-          style={styles.addButton}
+          style={[styles.addButton, {backgroundColor: this.getColor()}]}
           activeOpacity={0.7}
           onPress={() => this.setState({showAddTask: true})}>
           <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
@@ -222,7 +251,6 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: commonStyles.colors.today,
     justifyContent: 'center',
     alignItems: 'center',
   },
